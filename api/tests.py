7f02c60492cb36563ff10a7f1dfc5d376e2050fb
Joshua Blake <joshbblake@gmail.com>
2012-09-02 14:21:33 +0100

    Basic cache testing.
    Starting to use TDD (especially as this is being coded to an API).

--- api/tests.py
+++ api/tests.py
@@ -11,7 +11,12 @@ from eveapi import _RootContext
 class WrapperTest(TestCase):
     def setUp(self):
         self.api = get_api()
+        self.cache = self.api._root._handler
     
     def test_api_returned(self):
         """Test that method call returns an instance of the base eveapi class."""
         self.assertIsInstance(self.api, _RootContext)
+        
+    def test_cache_implemented(self):
+        """Test that cache is implemented."""
+        self.assertIsNotNone(self.cache)
