5b6a42122e131ae315cd4cbd4a5120742dba8148
Joshua Blake <joshbblake@gmail.com>
2012-09-02 14:28:32 +0100

    Update docstrings.
    Docstrings are now in compliance with PEP 257 and are actually useful!

--- api/api.py
+++ api/api.py
@@ -3,10 +3,25 @@
 Single way to retrieve underlying library. This allows the library to be easily
 replaced in future. We also implement a custom Cache handler using Django's
 caching backend.
+
+Functions:
+    `get_api()`: Return connection to the EvE API.
+
+Copyright 2012 Joshua Blake
+
 """
+
 from eveapi import EVEAPIConnection
 
 def get_api():
-    """Return an instance of the eveapi
+    """Return connection to the EvE API.
+    
+    Uses the `eveapi` library. The library is created. By consolidating all
+    initialisations here we make it easier to install mocks and if ever needed
+    implement a full wrapper.
+    
+    Returns:
+        An instance of `eveapi._RootContext`
+    
     """
     return EVEAPIConnection()
--- api/tests.py
+++ api/tests.py
@@ -1,7 +1,11 @@
-"""Test basic functionality"""
+"""Test basic functionality.
 
-from django.test import TestCase
+Copyright 2012 Joshua Blake
+
+"""
+
 from api import get_api
+from django.test import TestCase
 from eveapi import _RootContext
 
 class WrapperTest(TestCase):
@@ -9,5 +13,5 @@ class WrapperTest(TestCase):
         self.api = get_api()
     
     def test_api_returned(self):
-        """Test that method call returns an instance of the base eveapi class"""
+        """Test that method call returns an instance of the base eveapi class."""
         self.assertIsInstance(self.api, _RootContext)
